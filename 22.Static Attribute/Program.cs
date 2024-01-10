60e9e066c1ceb49052477de1ba5501b52df67e94
A253875 <A253875@LSBSA-65LG3J3>
2024-01-10 16:59:23 +0200

    Add project files.

---
+++ 22.Static Attribute/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Static attribute is a shows all that is shared by the objects of that class
+            //its an attribute on the class itself not on the individual members of the class, it is about the class
+            Songs holiday = new Songs("Holiday", "Green Day", 200);
+            Songs kashmir = new Songs("Kashmir", "Led Zeppelin", 150);
+            Console.WriteLine(Songs.songCount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 22.Static Attribute/Songs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Attribute
+{
+    internal class Songs
+    {
+        public string title; 
+        public string artist;
+        public int duration;
+        
+        public static int songCount = 0; //declaration of the static attribute
+        public Songs(string aTitle, string aArtist, int aDuration)
+        {
+            this.title = aTitle;
+            this.artist = aArtist;
+            this.duration = aDuration;
+            songCount++;//increment eact time a song is added
+        }
+    }
+}
